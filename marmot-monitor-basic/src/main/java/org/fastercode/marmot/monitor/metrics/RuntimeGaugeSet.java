19f0bfcf5a7480ac9aa6488a7481d791e6884f5a
huyaolong <mail@yaolong.net>
2021-01-15 18:14:06 +0800

    init

--- marmot-monitor-basic/src/main/java/org/fastercode/marmot/monitor/metrics/RuntimeGaugeSet.java
+++ marmot-monitor-basic/src/main/java/org/fastercode/marmot/monitor/metrics/RuntimeGaugeSet.java
@@ -6,6 +6,7 @@ import com.codahale.metrics.MetricSet;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -60,6 +61,7 @@ public class RuntimeGaugeSet implements MetricSet {
 
         gauges.put("uptime", (Gauge<Long>) runtime::getUptime);
         gauges.put("start.timestamp", (Gauge<Long>) runtime::getStartTime);
+        gauges.put("start.date", (Gauge<String>) () -> (new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")).format(new Date(runtime.getStartTime())));
 
         gauges.put("properties", (Gauge<Set<String>>) () -> systemProperties);
         gauges.put("arguments", (Gauge<List<String>>) runtime::getInputArguments);
