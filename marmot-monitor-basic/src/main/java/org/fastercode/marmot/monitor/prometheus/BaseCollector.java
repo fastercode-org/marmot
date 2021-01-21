6cc0855550bd1babd32dfa6ba470d94bef6322d5
huyaolong <mail@yaolong.net>
2021-01-21 19:05:56 +0800

    init

--- marmot-monitor-basic/src/main/java/org/fastercode/marmot/monitor/prometheus/BaseCollector.java
+++ marmot-monitor-basic/src/main/java/org/fastercode/marmot/monitor/prometheus/BaseCollector.java
@@ -5,8 +5,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.fastercode.marmot.core.properties.CoreProperties;
 import org.fastercode.marmot.core.properties.CorePropertyKey;
+import org.fastercode.marmot.util.IpUtil;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -15,19 +17,24 @@ import java.util.List;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public abstract class BaseCollector extends Collector {
+    private static final Collection<String> IP = IpUtil.getAllNoLoopbackAddresses();
+    private static final int START_POS = 3;
+
     protected List<String> labelNames(String... strings) {
-        String[] elements = new String[2 + strings.length];
+        String[] elements = new String[START_POS + strings.length];
         elements[0] = "project";
         elements[1] = "env";
-        System.arraycopy(strings, 0, elements, 2, strings.length);
+        elements[2] = "ip";
+        System.arraycopy(strings, 0, elements, START_POS, strings.length);
         return Arrays.asList(elements);
     }
 
     protected List<String> labelValues(String... strings) {
-        String[] elements = new String[2 + strings.length];
+        String[] elements = new String[START_POS + strings.length];
         elements[0] = project();
         elements[1] = env();
-        System.arraycopy(strings, 0, elements, 2, strings.length);
+        elements[2] = ip();
+        System.arraycopy(strings, 0, elements, START_POS, strings.length);
         return Arrays.asList(elements);
     }
 
@@ -37,6 +44,16 @@ public abstract class BaseCollector extends Collector {
 
     private String env() {
         return CoreProperties.get(CorePropertyKey.ENV);
+    }
+
+    private String ip() {
+        if (IP == null || IP.size() == 0) {
+            return "null";
+        }
+        if (IP.size() == 1) {
+            return String.valueOf(IP.toArray()[0]);
+        }
+        return Arrays.toString(IP.toArray());
     }
 
 }
