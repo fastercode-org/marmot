56976f2783d6aee61c018983e3211dc0e297f414
huyaolong <mail@yaolong.net>
2021-01-21 18:44:03 +0800

    init

--- marmot-monitor-basic/src/main/java/org/fastercode/marmot/monitor/prometheus/BaseCollector.java
+++ marmot-monitor-basic/src/main/java/org/fastercode/marmot/monitor/prometheus/BaseCollector.java
@@ -6,12 +6,37 @@ import lombok.EqualsAndHashCode;
 import org.fastercode.marmot.core.properties.CoreProperties;
 import org.fastercode.marmot.core.properties.CorePropertyKey;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author huyaolong
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
 public abstract class BaseCollector extends Collector {
-    private String project = CoreProperties.get(CorePropertyKey.PROJECT);
-    private String env = CoreProperties.get(CorePropertyKey.ENV);
+    protected List<String> labelNames(String... strings) {
+        String[] elements = new String[2 + strings.length];
+        elements[0] = "project";
+        elements[1] = "env";
+        System.arraycopy(strings, 0, elements, 2, strings.length);
+        return Arrays.asList(elements);
+    }
+
+    protected List<String> labelValues(String... strings) {
+        String[] elements = new String[2 + strings.length];
+        elements[0] = project();
+        elements[1] = env();
+        System.arraycopy(strings, 0, elements, 2, strings.length);
+        return Arrays.asList(elements);
+    }
+
+    private String project() {
+        return CoreProperties.get(CorePropertyKey.PROJECT);
+    }
+
+    private String env() {
+        return CoreProperties.get(CorePropertyKey.ENV);
+    }
+
 }
