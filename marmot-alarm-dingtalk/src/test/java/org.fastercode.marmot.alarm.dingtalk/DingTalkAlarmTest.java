f67cac0b83a234bf5221ce06e48871627f59b731
huyaolong <mail@yaolong.net>
2021-01-21 18:11:59 +0800

    init

--- marmot-alarm-dingtalk/src/test/java/org.fastercode.marmot.alarm.dingtalk/DingTalkAlarmTest.java
+++ marmot-alarm-dingtalk/src/test/java/org.fastercode.marmot.alarm.dingtalk/DingTalkAlarmTest.java
@@ -1,20 +1,4 @@
 package org.fastercode.marmot.alarm.dingtalk;
 
-import lombok.SneakyThrows;
-import org.fastercode.marmot.alarm.dingtalk.ding.DingtalkClientUtil;
-import org.fastercode.marmot.alarm.dingtalk.ding.message.TextMessage;
-import org.junit.Ignore;
-import org.junit.Test;
-
-import java.util.Arrays;
-
 public class DingTalkAlarmTest {
-    @Test
-    @Ignore
-    @SneakyThrows
-    public void test() {
-        TextMessage message = new TextMessage("订单测试");
-        message.setAtMobiles(Arrays.asList("18515666600"));
-        DingtalkClientUtil.send("https://oapi.dingtalk.com/robot/send?access_token=322da78a9750364b72e4b6cf68e545653cb4aa8fc8b36d2ca66fab8b4d555520", message);
-    }
 }
